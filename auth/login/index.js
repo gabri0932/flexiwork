828a644165028dd60aaad09ead302c9e6316595b
gabri0932 <gabrielgarces2432@gmail.com>
2025-03-22 15:55:42 +0100

    validaciones

--- auth/login/index.js
+++ auth/login/index.js
@@ -10,4 +10,15 @@ loginForm.addEventListener('submit', (event) => {
     );
 
     console.log(inputs);
+    //valido el correo y la contraseña
+    const {email, password} = inputs
+    const error = document.getElementById("id")
+    error.value = null
+    if(!email || !password){
+        email.value = "Email o contraseña vacios o nulos"
+        
+    }if(email.include('@') || email.includes('gmail.com')){
+        email.value = "Correo Invalido"
+    }
+    
 });
