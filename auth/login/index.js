789dccc34de13ebd2191acdcccf60439470873b4
Darlin Arias <darlin.code@gmail.com>
2025-03-22 11:11:22 -0400

    Changes done.

--- auth/login/index.js
+++ auth/login/index.js
@@ -1,24 +1,30 @@
 const loginForm = document.getElementById('login-form');
+const errorContainer = document.getElementById("error");
+let error = null;
 
 loginForm.addEventListener('submit', (event) => {
     // Para detener el envío del formulario.
     event.preventDefault();
+    error = null;
 
     // Para extraer los inputs del formulario en un objeto.
     const inputs = Object.fromEntries(
         new FormData(event.target)
     );
 
-    console.log(inputs);
-    //valido el correo y la contraseña
-    const {email, password} = inputs
-    const error = document.getElementById("id")
-    error.value = null
-    if(!email || !password){
-        email.value = "Email o contraseña vacios o nulos"
-        
-    }if(email.include('@') || email.includes('gmail.com')){
-        email.value = "Correo Invalido"
+    const { email, password } = inputs;
+
+    // Validación del correo y la contraseña.
+    (!email || !password) ? error = 'El correo y la contraseña son obligatorios.' : null;
+    (!email.includes('@') || !email.includes('gmail.com')) ? error = 'El correo es inválido.' : null;
+
+    // Para mostrar el error en pantalla en caso de haberlo.
+    if (error) {
+        errorContainer.innerText = error;
+        errorContainer.hidden = false;
+        return;
     }
-    
+
+    errorContainer.innerText = '';
+    errorContainer.hidden = true;
 });
