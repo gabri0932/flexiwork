99d9a0f915bdf6d898f15474513c44da5a361ebd
gabri0932 <gabrielgarces2432@gmail.com>
2025-04-03 23:36:05 +0200

    viusu

--- app/profile/view_usu/viuwusu.js
+++ app/profile/view_usu/viuwusu.js
@@ -1,18 +1,27 @@
 import { isAuth } from "../../../auth/scripts/auth"
 
-const getprofileEndpoint = 'https://api-rest-emprendi.onrender.com/profiles/:id'
+const getprofileEndpoint = 'https://api-rest-emprendi.onrender.com/profiles/'
 const params = new URLSearchParams(window.location.search)
-const valor = params.get("profile")
+const valor = params.get("profile") //recuperamos el valor de la url 
+if(!valor){
+    console.log("Error con la busqueda del parametro")
+}else{
+    console.log(valor)
+}
 (async ()=>{
 
-    const {isUsertAuht, session } = await isAuth()
-    if(!isUsertAuht) return;
-    const response = await fetch(getProfileEndpoint, {
+    const {isUserAuth, session } = await isAuth()
+    if(!isUserAuth) return;
+    const response = await fetch(`${getprofileEndpoint}/${valor}`, {
         headers: {
             Authorization: `Bearer ${session}`
         }
     });
     if(!response.ok) return;
+    const json = await response.json()
+    console.log(json)
+    
+
 })
 
  
